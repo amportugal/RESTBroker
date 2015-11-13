c29c16508faaf625fc0ff6489fe892f9afe15cf4
amportugal <nunommarques@ua.pt>
2015-11-13 14:27:14 +0000

    Changed documentation.

--- RESTModule.py
+++ RESTModule.py
@@ -78,11 +78,58 @@ def registerUser():
 
 #EditUser
 @app.route('/user/', methods=['PUT'])
-def editUser():
+def editUserInfo():
     """
     Edit user
     Edits information of a user.
+    Expects user_id.
     Expects the following JSON:
+    {
+        "latitude": 9.123
+        "longitude": 8.123
+        "interests": ["futebol", "tetris"]
+    }
+
+    ---
+    tags:
+      - User
+    responses:
+      200:
+        description: A single user item
+        schema:
+          id: return_json
+          properties:
+            code:
+              type: integer
+              description: The HTTP response code
+              default: '200'
+            reason:
+              type: string
+              description: The reason of not updating a user
+              default: ''
+    """
+    #Obtain JSON from message
+    json_msg=request.data
+
+    #Location service
+    loc_rest_url='http://192.168.8.217:4180/api/user/' + json_msg['user_id'] + '/'
+    data = json_msg
+    response = requests.PUT(loc_rest_url, data)
+
+    if response.status_code!=httplib.OK:
+        response_json = {"code": httplib.FORBIDDEN, "reason": "Could not update user."}
+        return response_json
+
+    response_json = {"code": httplib.OK, "reason": 'none'}
+
+    return response_json
+
+@app.route('/user/', methods=['DELETE'])
+def removeUser():
+    """
+    Delete user
+    Deletes a user.
+    Expects: user_id.
 
     ---
     tags:
@@ -154,7 +201,6 @@ def login():
 
     return response_json
 
-
 #Create Event
 @app.route('/event/', methods=['POST'])
 def createEvent():
@@ -166,9 +212,8 @@ def createEvent():
         "longitude": 1.234,
         "latitude": 2.343,
         "title": "title",
-        "subtitle": "subtitle",
+        "subtitle": "subtitle", (opcional)
         "description": "event description",
-        "small_description": "swag",
         "beginning": "15-03-2004T00:24:00",
         "end": "15-03-2004T00:24:00", (opcional)
         "cost": 2, (opcional)
@@ -197,23 +242,28 @@ def createEvent():
               type: string
               description: The reason of a not-created event
               default: ''
+            event_id:
+              type: int
+              description: Event identification
+              default: '2'
     """
     #Obtain json message
     json_msg=request.data
 
-
     #Location service: send event creation
-    #rest_url='http://192.168.8.217:4180/api/event/'
-    #response = requests.post(rest_url, headers={"X-CSRFToken": "04cAmRuBNouFtoq6ZkXcqq7cVKXiW5rH", "Content-type" : "application/json"}, data=json_msg)
+    rest_url='http://192.168.8.217:4180/api/event/'
+    response = requests.post(rest_url, headers={"X-CSRFToken": "04cAmRuBNouFtoq6ZkXcqq7cVKXiW5rH", "Content-type" : "application/json"}, data=json_msg)
 
     #print response.text
-    #if response.status_code!=httplib.OK:
-    #    return response
+    if response.status_code!=httplib.OK:
+        return response
 
     json_decoded=json.loads(json_msg)
-    #json_get_users={"latitude": json_decoded['latitude'], "longitude": json_decoded['longitude'], "interest": json_decoded['interest']}
-    #rest_url='http://192.168.8.217:4180/api/user/nearest&limit=' + str(json_decoded['min_people'])
-    #response = requests.post(rest_url, data=json_get_users)
+    json_get_users={"latitude": json_decoded['latitude'], "longitude": json_decoded['longitude'], "interest": json_decoded['interest']}
+    rest_url='http://192.168.8.217:4180/api/user/nearest&limit=' + str(json_decoded['min_people'])
+    response = requests.post(rest_url, data=json_get_users)
+
+    event_id=response.text
 
     #TODO: Extract reg_ids from the json_msg to variable data_to_send
     #json_users=json.loads(response.text)
@@ -231,7 +281,7 @@ def createEvent():
     rest_url='http://localhost:8080/sendEventCreateNotification'
     r = requests.get(url=rest_url, data=json.dumps(data_to_send))
 
-    response_json={"code": httplib.CREATED, "reason": "none"}
+    response_json={"code": httplib.CREATED, "reason": "none", "event_id": event_id}
 
 
     return response_json
@@ -242,22 +292,21 @@ def editEvent():
     """
     Edit event
     Edits an event.
-    Expects the id as parameter.
+    Expects the event id as parameter.
     Expects the following JSON:
     {
-        "longitude": 1.234,
-        "latitude": 2.343,
-        "title": "title",
-        "subtitle": "subtitle",
-        "description": "event description",
-        "small_description": "swag",
-        "beginning": "15-03-2004T00:24:00",
+        "longitude": 1.234, (opcional)
+        "latitude": 2.343, (opcional)
+        "title": "title", (opcional)
+        "subtitle": "subtitle", (opcional)
+        "description": "event description", (opcional)
+        "beginning": "15-03-2004T00:24:00", (opcional)
         "end": "15-03-2004T00:24:00", (opcional)
-        "cost": 2,
-        "type": False,
-        "min_people": 40,
-        "max_people": 100,
-        "interest": "Swag"
+        "cost": 2, (opcional)
+        "type": False, (opcional)
+        "min_people": 40, (opcional)
+        "max_people": 100, (opcional)
+        "interest": "Swag" (opcional)
     }
     ---
     tags:
@@ -324,7 +373,7 @@ def deleteEvent():
     """
     Delete event
     Delete an event.
-    Expects the id as parameter.
+    Expects the event_id as parameter.
 
     ---
     tags:
@@ -345,22 +394,21 @@ def deleteEvent():
               default: ''
     """
     #Obtain json message
-    json_msg=request.data()
+    event_id=request.data()
 
     #Location service: send event creation
-    #rest_url='http://192.168.8.217:4180/api/event/' + "30" + "/" #TODO: json_msg["event_id"]
-    #response = requests.delete(rest_url,  headers={"X-CSRFToken": "04cAmRuBNouFtoq6ZkXcqq7cVKXiW5rH", "Content-type" : "application/json"}, data="")
+    rest_url='http://192.168.8.217:4180/api/event/' + event_id + "/"
+    response = requests.delete(rest_url,  headers={"X-CSRFToken": "04cAmRuBNouFtoq6ZkXcqq7cVKXiW5rH", "Content-type" : "application/json"})
 
 
-    #if response.status_code!=httplib.CREATED:
-    #    return response
+    if response.status_code!=httplib.CREATED:
+        return response
 
 
-    json_decoded=json.loads(json_msg)
-    #rest_url='http://192.168.8.217:4180/api/user/attending/event/' + "3" #TODO: json_msg["event_id"]
-    #response = requests.get(rest_url, headers={"X-CSRFToken": "04cAmRuBNouFtoq6ZkXcqq7cVKXiW5rH", "Content-type" : "application/json"}, data="")
+    rest_url='http://192.168.8.217:4180/api/user/attending/event/' + event_id + "/"
+    response = requests.get(rest_url, headers={"X-CSRFToken": "04cAmRuBNouFtoq6ZkXcqq7cVKXiW5rH", "Content-type" : "application/json"})
 
-    #static data
+    # TODO: static data
     reg_ids={"reg_ids" : ["APA91bFtxXJsZXTG8yHBmjW__PbXJ8NXClnr3p7ioUbR9M2IO1irQWhF30MF94-VBW4ixd4JABl6_mj-4XOvfkSYPupXyL25WIje3V7T7L7lHBeHZRmBYvuLGHLu5wOZy3X3Au8Qs7Z_"]}
 
     data_to_send = {key: value for (key, value) in (reg_ids.items() + json_decoded.items())}
@@ -403,7 +451,6 @@ def searchEvent():
                     "title": "title",
                     "subtitle": "subtitle",
                     "description": "event description",
-                    "small_description": "swag",
                     "beginning": "15-03-2004T00:24:00",
                     "end": "15-03-2004T00:24:00", (opcional)
                     "cost": 2,
@@ -413,7 +460,61 @@ def searchEvent():
                     "interest": "Swag"
                 }'
     """
-    pass
+    #Obtain event_id
+    event_id=request.data()
+
+    #Location service: send event creation
+    rest_url='http://192.168.8.217:4180/api/event/' + event_id + '/'
+    response = requests.get(rest_url, headers={"X-CSRFToken": "04cAmRuBNouFtoq6ZkXcqq7cVKXiW5rH", "Content-type" : "application/json"})
+
+    if response.status_code!=httplib.OK:
+        return response
+
+    response_json={"code": httplib.OK, "reason": "none", "info": response.text}
+
+    return response_json
+
+
+#Get Nearest Events
+@app.route('/getNearestEvents/', methods=['GET'])
+def getNearestEvents():
+    """
+    Get Nearest events
+    Obtains all the events near the user.
+    Expects the user_id as parameter.
+
+    ---
+    tags:
+      - Event
+    responses:
+      200:
+        description: A single user item
+        schema:
+          id: return_json
+          properties:
+            code:
+              type: integer
+              description: The HTTP response code
+              default: '200'
+            info:
+              type: string
+              description: All the events info.
+              default: 'e.g. http://pastebin.com/tk0TncXu'
+    """
+
+    #Obtain event_id
+    user_id=request.data()
+
+    #Location service: send event creation
+    rest_url='http://192.168.8.217:4180/api/event/nearest' + user_id + '/'
+    response = requests.get(rest_url, headers={"X-CSRFToken": "04cAmRuBNouFtoq6ZkXcqq7cVKXiW5rH", "Content-type" : "application/json"})
+
+    if response.status_code!=httplib.OK:
+        return response
+
+    response_json={"code": httplib.OK, "reason": "none", "info": response.text}
+
+    return response_json
 
 #Join User to Event
 @app.route('/joinUserToEvent/', methods=['POST'])
@@ -438,25 +539,25 @@ def joinUserToEvent():
               default: '200'
             reason:
               type: string
-              description: The reason of a not-created event
+              description: The reason of user not joining an event
               default: ''
     """
-    # json_msg=web.data()
-    # json_decoded = json.loads(json_msg)
+
+    json_decoded = json.loads(request.data)
 
     # get user id from joao
-    # user_id = json_decoded['user_id']
-    user_id = 1
+    user_id = json_decoded['user_id']
+    #user_id = 1
 
     # get event id from johny boy
-    # event_id = json_decoded['event_id']
-    event_id = 19
+    event_id = json_decoded['event_id']
+    #event_id = 19
 
     # do the joiningz man
     rest_url='http://192.168.8.217:4180/api/event/attending/' + str(user_id) + '/'
     response = requests.request('PUT', rest_url, data={'event_id': event_id})
 
-    response_json={"code": httplib.OK, "reason": "none"}
+    response_json={"code": httplib.OK, "reason": "none", "info": response.text}
     return response_json
 
 #Delete User to Event
@@ -482,25 +583,25 @@ def deleteUserToEvent():
               default: '200'
             reason:
               type: string
-              description: The reason of a not-created event
+              description: The reason of user not being able to leave the event
               default: ''
     """
     # json_msg=web.data()
-    # json_decoded = json.loads(json_msg)
+    json_decoded = json.loads(request.data)
 
     # get user id from joao
-    # user_id = json_decoded['user_id']
-    user_id = 1
+    user_id = json_decoded['user_id']
+    # user_id = 1
 
     # get event id from johny boy
-    # event_id = json_decoded['event_id']
-    event_id = 19
+    event_id = json_decoded['event_id']
+    #event_id = 19
 
 
     # do the deletingz man
     rest_url='http://192.168.8.217:4180/api/event/attending/' + str(user_id) + '/'
     response = requests.request('DELETE', rest_url, data={'event_id': event_id})
-    response_json={"code": httplib.OK, "reason": "none"}
+    response_json={"code": httplib.OK, "reason": "none", "info" : response.text}
     return response_json
 
 #Get Users Near Event
@@ -520,17 +621,20 @@ def getUsersNearEvent():
         schema:
           id: return_test
           properties:
-            result:
+            code:
+              type: integer
+              description: The HTTP response code
+              default: '200'
+            info:
               type: string
-              description: The test
-              default: 'test'
+              description: All the users info.
+              default: 'e.g. http://pastebin.com/WUCFmedg'
     """
-    # json_msg=web.data()
-    # json_decoded = json.loads(json_msg)
+
 
     # get event id from johny boy
-    # event_id = json_decoded['event_id']
-    event_id = 19
+    event_id = request.data
+    # event_id = 19
 
 
     print event_id
@@ -554,8 +658,8 @@ def getUsersNearEvent():
                                           'latitude': latitude,
                                           'interest': interest})
 
-    #response_json={"code": httplib.OK, "reason": "none"}
-    return response.text
+    response_json={"code": httplib.OK, "reason": "none", "info": response.text}
+    return response_json
 
 
 
